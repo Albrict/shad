2569f74a76e40f54417dc01430c4628e9c0826ed
Albrict <karelinaleksj@gmail.com>
2022-11-26 00:12:44 +0200

    Now supports save/load

--- shad.c
+++ shad.c
@@ -1,11 +1,66 @@
 #include "shad.h"
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
 
-int main ()
+/* filename */
+char *name = NULL;
+
+/* File functions */
+static void load()
 {
-    init_shad();
+    FILE *f = fopen(name, "r"); 
+    load_field_and_init(f);
+    fclose(f);
+}
+
+static void create()
+{
+    FILE *f = fopen(name, "w");
+    if (!f) {
+        die("Cant't create file.");
+    }
+    fclose(f);
+}
+
+void save()
+{
+    FILE *f = fopen(name, "w");
+    putwin(get_field(), f);
+    fclose(f);
+}
+
+void run()
+{
     while (true) {
         procces_input();
         update();
+    }
+}
+
+int main (int argc, char *argv[])
+{
+    /* Firstly we check, if we have arguments */
+    if (argc > 1) {
+        name = argv[1];
+        /* Then we check, if file exist */
+        if (access(name, F_OK) == 0) {
+            load();
+            run();
+        } else {
+            create();
+            init_shad();
+            run();
+        }
+    /* Else we create new file with time title */
+    } else {
+        time_t time;
+        char *current_time = ctime(&time);
+        name = current_time;
+        current_time[strlen(current_time) - 1] = '\0';
+        create();
+        init_shad();
+        run();
     }
     return 0;
 }
