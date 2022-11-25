161f8d06fb64ed6364ec2b9a5345a6a6c9c390bc
Albrict <karelinaleksj@gmail.com>
2022-11-26 00:12:15 +0200

    Upgraded GUI

--- screen.c
+++ screen.c
@@ -7,7 +7,7 @@ extern const char err_init_colors[];
 extern const char err_init_mouse[]; 
 
 const char msg_current_color[] = "Current color:";
-
+const char msg_current_tool[] = "Current tool:";
 /* Constants and macros */
 #define BAR_HEIGHT 6
 
@@ -29,6 +29,7 @@ WINDOW *get_bar()
 }
 
 /* Window functions */
+
 void update()
 {
     wrefresh(get_bar());
@@ -44,8 +45,35 @@ void end_screen()
 
 void set_current_color()
 {
-
+    static int x;
+    x = getmaxx(stdscr) - 15;
+    change_color(get_current_color(), get_bar());
+    mvwaddch(get_bar(), 1, x, ' ');
 }
+
+static void draw_pallete()
+{
+    colors i = RED;
+    int y = 3, x = getmaxx(get_field()) - 20;
+    int msg_x = getmaxx(get_field()) - sizeof(msg_current_color) - 15;
+    for (; i < COLOR_WHITE + 1; i++) {
+        if (x == getmaxx(get_bar()) - 1) {
+            y++;
+            x = 1;
+        }
+        change_color(i, get_bar());
+        mvwaddch(get_bar(), y, x, ' ');
+        x++;
+    }
+    change_color(BLACK, get_bar());
+    mvwaddstr(get_bar(), 1, msg_x, msg_current_color);
+}
+
+/*static void print_tools()
+{
+    int y = 3, x = 10; 
+    
+} */
 
 /* Initizalition */
 static void init_field()
@@ -55,26 +83,16 @@ static void init_field()
     keypad(get_field(), true);
 }
 
+
 static void init_bar()
 {
-    int i = COLOR_RED;
-    int y = 1, x = getmaxx(get_field()) - 10;
-    int msg_x = x - sizeof(msg_current_color) - 1;
     /* Height, width, starty, startx */
     tool_bar = newwin(BAR_HEIGHT, COLS, LINES - 6, 0); 
     box(get_bar(), 0, 0);
 
     /* Now time to draw our color pallete */
-    for (; i < COLOR_WHITE + 1; i++) {
-        if (y == getmaxy(get_bar()) - 1) {
-            x++;
-            y = 1;
-        }
-        change_color(i, get_bar());
-        mvwaddch(get_bar(), y, x, ' ');
-        y++;
-    }
-    mvwaddstr(get_bar(), 1, msg_x, msg_current_color);
+    draw_pallete();
+    mvwaddstr(get_bar(), 1, 15, msg_current_tool);
 }
 
 static void init_mouse()
@@ -85,7 +103,7 @@ static void init_mouse()
     mouseinterval(0);
 }
 
-void init_shad()
+static void init_terminal()
 {
     if (!initscr()) 
         die(err_init_screen);
@@ -102,9 +120,24 @@ void init_shad()
     halfdelay(1);
     refresh();
 
+}
+
+void load_field_and_init(FILE *field)
+{
+    init_terminal();
+    main_field = getwin(field); 
+    init_bar();
+    init_mouse();
+
+    update();
+}
+
+void init_shad()
+{
+    init_terminal();
     init_field();
     init_bar();
     update();
-   
+    
     init_mouse();
 }
