7ad55e5f245328f18b1f3e8f8b52f0bf7a1cc521
Albrict <karelinaleksj@gmail.com>
2022-11-29 00:59:26 +0200

    Added new error message

--- error.c
+++ error.c
@@ -4,10 +4,11 @@
 const char err_init_screen[] = "Coudln't initialize ncurses\n";
 const char err_init_colors[] = "Coudln't initialize colors\n";
 const char err_init_mouse[] = "Coudln't initialize mouse\n";
+const char err_open_drawing[] = "Coudln't open file. File is too large.\n";
 
 void die(const char *error)
 {
-    printf("%s", error);
     end_screen();
+    printf("%s", error);
     exit(EXIT_FAILURE);
 }
